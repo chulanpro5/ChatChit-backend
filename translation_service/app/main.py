a0c23579a5ea5aa7fab351ccc3db76820076aa2b
chulanpro5 <chulanpro5@gmail.com>
2024-01-10 11:04:04 +0700

    fix: python translation service

--- translation_service/app/main.py
+++ translation_service/app/main.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from deep_translator import GoogleTranslator,single_detection
 
 app = FastAPI()
 
+class TranslationRequest(BaseModel):
+    text: str
+    target_language: str = 'vi'
+
 @app.post("/translate/")
-async def translate_text(text: str, target_language: str = 'vi'):
+async def translate_text(request: TranslationRequest):
+    text = request.text
+    target_language = request.target_language
     translated = GoogleTranslator(source='auto', target=target_language).translate(text)
     return {"translation": translated}
 
-@app.post("/detect/")
-async def detect_language(text: str, api_key: str = None):
-    detected = single_detection(text, api_key=api_key)
-    return {"language": detected}
+# @app.post("/detect/")
+# async def detect_language(text: str, api_key: str = None):
+#     detected = single_detection(text, api_key=api_key)
+#     return {"language": detected}
